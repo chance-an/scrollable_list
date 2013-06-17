3c8f06f19c2819af2d39559dde2dd4feed228c37
Changsi An <anchangsi@gmail.com>
2013-06-17 00:24:07 -0700

    Throttling the event handling rate of 'scroll' event to get smoother experience.

--- scrollable_list.js
+++ scrollable_list.js
@@ -195,9 +195,9 @@
 
     function _monitorEvents(self){
         self.$element.on('scroll', function(instance){
-            return function(){
+            return debounce(function(){
                 Events.onScroll.apply(instance.$element, [instance].concat($.makeArray(arguments)) );
-            };
+            }, 50);
         }(self));
     }
 
@@ -238,4 +238,23 @@
         }
         return null;
     };
+
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) {
+                    func.apply(context, args);
+                }
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) {
+                func.apply(context, args);
+            }
+        };
+    }
 })(jQuery);
